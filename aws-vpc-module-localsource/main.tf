5dc323e713b25e15ed13b3120a1245e155c24e3a
Simon <slynch@slynch-z01.localdomain>
2020-11-16 11:21:37 +1100

    update aws vpc

--- aws-vpc-module-localsource/main.tf
+++ aws-vpc-module-localsource/main.tf
@@ -4,16 +4,16 @@ module "vpc" {
   
   name = "simple-example"
 
-  cidr = "10.0.0.0/16"
+  cidr = "5.0.0.0/16"
 
   azs             = var.azs
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnets = ["5.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_ipv6 = true
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  enable_nat_gateway = false
+  single_nat_gateway = false
 
   public_subnet_tags = {
     Name = "overridden-name-public"
