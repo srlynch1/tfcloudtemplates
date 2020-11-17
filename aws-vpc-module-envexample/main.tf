6b1e0f268527b8f7ef29de84d1f62a4fa84ffa61
Simon <slynch@slynch-z01.localdomain>
2020-11-17 15:34:13 +1100

    aws environment examples

---
+++ aws-vpc-module-envexample/main.tf
@@ -0,0 +1,30 @@
+
+module "vpc" {
+  source  = "github.com/terraform-aws-modules/terraform-aws-vpc" 
+
+  name = var.name
+
+  cidr = "5.0.0.0/16"
+
+  azs             = var.azs
+  private_subnets = ["5.0.1.0/24", "5.0.2.0/24", "5.0.3.0/24"]
+  public_subnets  = ["5.0.101.0/24", "5.0.102.0/24", "5.0.103.0/24"]
+
+  enable_ipv6 = true
+
+  enable_nat_gateway = false
+  single_nat_gateway = false
+
+  public_subnet_tags = {
+    Name = "overridden-name-public"
+  }
+
+  tags = {
+    Owner       = "user"
+    Environment = "dev"
+  }
+
+  vpc_tags = {
+    Name = "vpc-name"
+  }
+}
---
+++ aws-vpc-module-envexample/variables.tf
@@ -0,0 +1,11 @@
+variable "azs" {
+  type    = list(string)
+  default = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"]
+
+}
+
+variable "name" {
+  type    = string
+  default = "test vpc"
+
+}
