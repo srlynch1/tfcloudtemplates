f50d009d13caf0d3750b0e72a9fbb6e33fa22d56
Simon <slynch@slynch-z01.localdomain>
2020-11-24 14:17:12 +1100

    aws-vpc

--- aws-vpc-withmodule/main.tf
+++ aws-vpc-withmodule/main.tf
@@ -6,18 +6,18 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.64.0"
 
-  name = "simple-example"
+  name = var.name
 
-  cidr = "10.0.0.0/16"
+  cidr = var.cidr
 
-  azs             = ["eu-west-1a", "eu-west-1b", "euw1-az3"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = var.azs
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
 
   enable_ipv6 = true
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  enable_nat_gateway = false
+  single_nat_gateway = false
 
   public_subnet_tags = {
     Name = "overridden-name-public"
@@ -29,6 +29,6 @@ module "vpc" {
   }
 
   vpc_tags = {
-    Name = "vpc-name"
+    Name = var.name
   }
 }
