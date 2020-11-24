b85c4bfa77a50f3a8f10bf548c1c6d30be059200
Simon <slynch@slynch-z01.localdomain>
2020-11-24 14:05:35 +1100

    update variables

--- aws-vpc-module-env/variables.tf
+++ aws-vpc-module-env/variables.tf
@@ -1,3 +1,8 @@
+variable "region" {
+  type    = string
+  default = "ap-southeast-2"
+}
+
 variable "azs" {
   type    = list(string)
   default = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"]
@@ -26,7 +31,3 @@ variable "public_subnets" {
 }
 
 
-variable "region" {
-  type    = string
-  default = "ap-southeast-2"
-}
