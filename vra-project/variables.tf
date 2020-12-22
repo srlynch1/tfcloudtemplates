889d0c39e1fb6cc7009c88652033ba30d3a76231
Simon Lynch <srlynch@gmail.com>
2020-12-22 12:30:14 +1100

    update desc

--- vra-project/variables.tf
+++ vra-project/variables.tf
@@ -7,16 +7,17 @@ variable "refresh_token" {
   sensitive = true
 }
 
-
 variable "zones" {
   type    = list(string)
   default = ["ANZ AWS APSE2", "ANZ Azure AustraliaEast", "SDDC ANZ vSphere / CM Lab"]
 }
 
 variable "project_name" {
+  default = "Demo - Terraform Created"
 }
 
 variable "project_desc" {
+  default = "created using Terraform Service"
 }
 
 variable "project_admins" {
